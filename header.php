68082882e78b23b8e143522aa25063651b8e771b
Naveen Singh <contact@naveensingh.net>
2014-12-01 23:13:13 +0530

    added css reset and other basic changes

--- header.php
+++ header.php
@@ -1,42 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="HandheldFriendly" content="True" />
+<meta name="MobileOptimized" content="320" />
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merriweather:300,700,700italic,300italic|Open+Sans:300" />
 
-<head profile="http://gmpg.org/xfn/11">
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
-	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
-
-	<style type="text/css" media="screen">
-		@import url( <?php bloginfo('stylesheet_url'); ?> );
-	</style>
-
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
-
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<?php
-    wp_get_archives('type=monthly&format=link');
-    wp_head();
-  ?>
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php wp_head(); ?>
 </head>
 
 <body>
-  <div id="canvas">
-
-    <ul class="skip">
-      <li><a href="#nav">Skip to navigation</a></li>
-      <li><a href="#primaryContent">Skip to main content</a></li>
-      <li><a href="#secondaryContent">Skip to secondary content</a></li>
-      <li><a href="#footer">Skip to footer</a></li>
-    </ul>
-
-    <div id="header">
-      <?php
-        // see this theme's functions.php
-        print do_heading();
-        print naked_nav($post);
-      ?>
+  <header>
+    <div class="container">
+        <div class="inner">
+            <a class="blog-logo" href='<?php echo esc_url( home_url( '/' ) ); ?>' rel='home'>
+              <?php bloginfo( 'name' ); ?>
+            </a>
+        </div>
     </div>
-
-    <div id="primaryContent">
+ </header>
+<div class="container">
