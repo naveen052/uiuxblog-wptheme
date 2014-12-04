b48580329d29966c6b671519149aa7845f0b021f
Naveen Singh <contact@naveensingh.net>
2014-12-04 22:44:42 +0530

    fixed sidebar

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
       <div class="col-md-4 sidebar">
-        <div class="loop">
+        <div class="loop" id="sidebar">
             <?php get_sidebar(); ?>
         </div>
       </div>
@@ -14,6 +14,22 @@
         <p class="pull-left">// Lorem ipsum dolor sit amet</p>
       </footer>
 
+<script>
+    $(document).ready(function() {
+      $(window).scroll(function () {
+          //if you hard code, then use console
+          //.log to determine when you want the
+          //nav bar to stick.
+          console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 100) {
+          $('#sidebar').addClass('sidebar-fixed');
+        }
+        if ($(window).scrollTop() < 101) {
+          $('#sidebar').removeClass('sidebar-fixed');
+        }
+      });
+    });
+</script>
 
   </body>
 </html>
